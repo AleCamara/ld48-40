8f99178f789162a373cde175103c54bd939b97d5
Alejandro Cámara <a.camara.iglesias@gmail.com>
2017-12-02 12:08:12 +0000

    Return character to reception if dropping it outside of a room

--- ld40-unity/Assets/Scripts/Input/CharacterInputReceiver.cs
+++ ld40-unity/Assets/Scripts/Input/CharacterInputReceiver.cs
@@ -2,10 +2,24 @@
 
 public class CharacterInputReceiver : BaseInputReceiver
 {
+    private static readonly int MAX_NUM_RAYCAST_HITS = 3;
+
     public Transform characterRoot = null;
+    public float raycastDistance = 100;
+    public LayerMask raycastLayerMask = new LayerMask();
+
+    private Vector3 _initialPosition = Vector3.zero;
+    private RaycastHit2D[] _raycastResults = null;
+
+    private void Start()
+    {
+        _raycastResults = new RaycastHit2D[MAX_NUM_RAYCAST_HITS];
+    }
 
     public override void OnTouchBegan(TouchEventInfo eventInfo)
-    { }
+    {
+        _initialPosition = characterRoot.position;
+    }
 
     public override void OnTouchMoved(TouchEventInfo eventInfo)
     {
@@ -15,5 +29,13 @@ public class CharacterInputReceiver : BaseInputReceiver
     }
 
     public override void OnTouchEnded(TouchEventInfo eventInfo)
-    { }
+    {
+        // Check if there's a room below
+        Ray ray = Camera.main.ScreenPointToRay(eventInfo.touchPositionPix);
+        int numHits = Physics2D.RaycastNonAlloc(ray.origin, ray.direction, _raycastResults, raycastDistance, raycastLayerMask);
+        if (numHits == 0)
+        {
+            characterRoot.position = _initialPosition;
+        }
+    }
 }
